41ac7c31ad67fe5fabebcde3471c7963cd825b94
Helen9229 <110952802+Helen9229@users.noreply.github.com>
2022-08-26 08:51:39 -0300

    Add files via upload

---
+++ ADA1.py
@@ -0,0 +1,13 @@
+
+#programadora:Helen Garcia
+#curso de programacion: en ADA 
+#lenguaje: python
+#objetivo:Crear un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
+
+
+nombre = input("ingrese su nombre: ")
+apellido = input("ingrese su apellido: ")
+mensaje = "hola" +" "+ nombre +"," + apellido +" " +"Bienvenida a ADA"
+
+print(mensaje)
+
